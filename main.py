e0f1bc07a7369adafdb1d704ccb5259bad746c7a
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2021-09-30 08:51:01 -0400

    Fixed rfftfreq parameter
    Forget that dt was space between samples, not Hz, so didn't need to be reciprocated.

--- main.py
+++ main.py
@@ -8,19 +8,19 @@ from scipy.fft import rfft, rfftfreq, irfft
 import numpy as np
 
 
-dt = 0.001 # sample rate
-t = np.arange(0,1,dt) # duration of sampling
+dt = 0.001
+t = np.arange(0,1,dt)
 f = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*120*t) #array
 f_original = f
-f = f + 2.5*np.random.randn(len(t)) # random noisy signal
+f = f + 2.5*np.random.randn(len(t))
 f_noisy = f
 
 # Number of samples
 n = len(t)
 
-fhat = rfft(f) # complex array of freq domain
-freq = rfftfreq(n, 1 / dt) # arr
-power_spectral_density = fhat * np.conj(fhat) / n # remove complex numbers
+fhat = rfft(f) # complex array
+freq = rfftfreq(n, dt) # arr of bin centers
+power_spectral_density = fhat * np.conj(fhat) / n # arr
 
 PSDfilter = power_spectral_density > 100 # create bool arr of non-noisy idx's
 PSD = power_spectral_density * PSDfilter
