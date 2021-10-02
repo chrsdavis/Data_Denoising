9075c68e1d73fb41db30c4d4454f79965ff643ae
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2021-10-02 15:29:29 -0400

    Defined functions
    Split up simple code into discrete functions; added basic graphing utilities. Need to rethink what exactly each function needs to do, rather clunky.

--- main.py
+++ main.py
@@ -8,35 +8,51 @@ from scipy.fft import rfft, rfftfreq, irfft
 import numpy as np
 
 
-dt = 0.001
-t = np.arange(0,1,dt)
-f = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*120*t) #array
-f_original = f
-f = f + 2.5*np.random.randn(len(t))
-f_noisy = f
+def clean_data(f_noisy,n,dt,lim):
+  fhat = rfft(f_noisy) # complex array
+  power_spectral_density = fhat * np.conj(fhat) / n # arr
+  PSDfilter = power_spectral_density > lim # create bool arr of non-noisy idx's
+  fhat = PSDfilter * fhat
+  return irfft(fhat)
 
-# Number of samples
-n = len(t)
+def gen_signal_clean(dt):
+  t = np.arange(0,1,dt)
+  f = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*120*t) #array
+  return f
 
-fhat = rfft(f) # complex array
-freq = rfftfreq(n, dt) # arr of bin centers
-power_spectral_density = fhat * np.conj(fhat) / n # arr
+def gen_signal_noisy(dt):
+  t = np.arange(0,1,dt)
+  f = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*120*t) #array
+  f_noisy = f + 2.5*np.random.randn(len(t))
+  return f_noisy
 
-PSDfilter = power_spectral_density > 100 # create bool arr of non-noisy idx's
-PSD = power_spectral_density * PSDfilter
-temp = fhat # temp var for graphing
-fhat = PSDfilter * fhat
+def plot_noisy(dt):
+  t = np.arange(0,1,dt)
+  f = np.sin(2*np.pi*50*t) + np.sin(2*np.pi*120*t) #array
+  f_noisy = f + 2.5*np.random.randn(len(t))
+  plt.plot(t,f_noisy)
+  plt.show()
 
-fclean = irfft(fhat)
+def plot_freq_domain(f_noisy,n,dt):
+  fhat = rfft(f_noisy) # complex array
+  freq = rfftfreq(n, dt) # arr of bin centers
+  plt.plot(freq,np.abs(fhat)) # original freq domain
+  plt.show()
 
-plt.plot(t,f_noisy) # original 
-plt.show()
+def plot_freq_domain_filt(f_noisy,n,dt,lim):
+  fhat = rfft(f_noisy) # complex array
+  freq = rfftfreq(n, dt) # arr of bin centers
+  power_spectral_density = fhat * np.conj(fhat) / n # arr
+  PSDfilter = power_spectral_density > lim # create bool arr of non-noisy idx's
+  fhat = PSDfilter * fhat
+  plt.plot(freq,np.abs(fhat)) # filtered freq domain
+  plt.show()
 
-plt.plot(freq,np.abs(temp)) # original freq domain
-plt.show()
-
-plt.plot(freq,np.abs(fhat)) # filtered freq domain
-plt.show()
-
-plt.plot(t,fclean) # filtered signal
-plt.show()
+def plot_clean_signal(f_noisy,n,dt,lim):
+  fhat = rfft(f_noisy) # complex array
+  power_spectral_density = fhat * np.conj(fhat) / n # arr
+  PSDfilter = power_spectral_density > lim # create bool arr of non-noisy idx's
+  fhat = PSDfilter * fhat
+  fclean = irfft(fhat)
+  plt.plot(fclean) # filtered signal
+  plt.show()
